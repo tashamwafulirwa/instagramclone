e18ec830e9d7591aed5c24af1c4781f7246060f9
natashamwafulirwa <natasha.mwafulirwa@nivohub.com>
2023-03-23 20:46:44 +0000

    add scrollview feature

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, ScrollView, Text, View, TextInput } from 'react-native';
 
@@ -18,63 +17,68 @@ function confirmPasswordsMatch(props) {
 }
 
 return (
-  <View style={{
-    flex: 1,
-    justifyContent:'center',
-    backgroundColor:'#ecf0f1' }}>
-       <InputWithLabel
+  <ScrollView contentContainerStyle={styles.scrollViewContentContainer}>
+    <InputWithLabel
       label="Name"
       placeholder="Type your name here"
       value={name}
       onChangeText={setName}>
-      </InputWithLabel>
-      <InputWithLabel
+    </InputWithLabel>
+    <InputWithLabel
       label="Date of birth"
       placeholder="Type your birthday here"
       value={birth}
       onChangeText={setBirth}>
-      </InputWithLabel>
-      <InputWithLabel
+    </InputWithLabel>
+    <InputWithLabel
       label="Email"
       placeholder="Type your email here"
       value={email}
       onChangeText={setEmail}/>
-      <InputWithLabel
+    <InputWithLabel
       label="Password"
       placeholder="Type your password here"
       value={password}
       onChangeText={setPassword}
-      secureTextEntry={true}
-      >
-      </InputWithLabel>
-      <InputWithLabel
+      secureTextEntry={true}>
+    </InputWithLabel>
+    <InputWithLabel
       label="Confirm password"
       placeholder="Confirm your password here"
       value={confirmationPassword}
       onChangeText={setConfirmationPassword}
       secureTextEntry={true}
-      onSubmitEditing={confirmPasswordsMatch}
-      >
-      </InputWithLabel>
-   </View>
-)
-  }
+      onSubmitEditing={confirmPasswordsMatch}>
+    </InputWithLabel>
+  </ScrollView>
+);
+}
 
-  function InputWithLabel(props) {
-    const { label, placeholder, value, onChangeText, secureTextEntry, onSubmitEditing } = props;
-    return (
-      <View style={{ padding: 16}}>
-        <Text style={{ padding: 8, fontSize: 18 }}>{label}</Text>
-        <TextInput
-        placeholder={placeholder}
-        value={value}
-        onChangeText={onChangeText}
-        secureTextEntry={secureTextEntry}
-        onSubmitEditing={onSubmitEditing}
-        style={{ padding: 8, fontSize: 18 }}>
-        </TextInput>
-      </View>
-    );
-  }
+function InputWithLabel(props) {
+const { label, placeholder, value, onChangeText, secureTextEntry, onSubmitEditing } = props;
+return (
+  <View style={{ padding: 16}}>
+    <Text style={{ padding: 8, fontSize: 18 }}>{label}</Text>
+    <TextInput
+      placeholder={placeholder}
+      value={value}
+      onChangeText={onChangeText}
+      secureTextEntry={secureTextEntry}
+      onSubmitEditing={onSubmitEditing}
+      style={{ padding: 8, fontSize: 18 }}>
+    </TextInput>
+  </View>
+);
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:'#ecf0f1',
+  },
+  contentContainer: {
+    paddingBottom: 2000, // set a large enough padding bottom value to allow for scrolling
+  },
+});
 
 export default App;
